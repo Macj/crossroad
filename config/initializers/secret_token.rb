89c4f90b248608ac2978d7de62d3d652b800d72e
Max Bulgakov <madmacj@gmail.com>
2013-10-01 13:34:44 +0300

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crossroad::Application.config.secret_token = '0fa8d4cd856729e1e06b0ed1265b35952b43f3ac1a000c9190b50acf4a38417e18fd543ec3580c435b820f40283dec75d1069297cb2180da67424c47bb883a07'
