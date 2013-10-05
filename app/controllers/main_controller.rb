dab34696ef48b88f42248399442916dce6e8a820
Max Bulgakov <madmacj@gmail.com>
2013-10-05 11:49:11 +0300

    t1 main page stucture *done

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,21 @@
 #encoding: utf-8
 class MainController < ApplicationController
   #protect_from_forgery
+  SECTIONS = {
+    'Article' => 'Статьи',
+    'Event'   => 'События',
+    'Place'   => 'Места',
+    'Project' => 'Проекты',
+    'Person'  => 'Лица',
+  }
 
-   def index
-   	puts Category.main.inspect
+  def index
     @categories = Category.main
+    @sections = SECTIONS
   end
+
+  def section
+    @section = SECTIONS.select{|k,v| k == params[:section]}.first
+  end  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -62,5 +62,6 @@ Crossroad::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
   match "template", :controller => 'main', :action => 'template'
+  match "section/(:section)", :controller => 'main', :action => 'section', :as => :section
 
 end
