86c4857879343569ffd4ddcf787c9aa9c3bb3c4e
Max Bulgakov <madmacj@gmail.com>
2013-10-08 23:24:14 +0300

    add article to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Category.create(:name => 'Культура')
-Category.create(:name => 'Театр', :parent_id => 1)
-Category.create(:name => 'Кино', :parent_id => 1)
-Category.create(:name => 'Музыка', :parent_id => 1)
+#Category.create(:name => 'Культура')
+#Category.create(:name => 'Театр', :parent_id => 1)
+#Category.create(:name => 'Кино', :parent_id => 1)
+#Category.create(:name => 'Музыка', :parent_id => 1)
+
+Type.create(:name => 'test article type')
+Article.create( name: "Test", category_id: 2, autor_id: nil, body: "Test body for test article", sources: 'test.article.dp.ua',  autor: 'Crossroad', type_id: 1)
+Article.create( name: "Test2", category_id: 2, autor_id: nil, body: "Test body for test article2", sources: 'test.article2.dp.ua',  autor: 'Crossroad', type_id: 1)
+Article.create( name: "Test3", category_id: 2, autor_id: nil, body: "Test body for test article3", sources: 'test.article3.dp.ua',  autor: 'Crossroad', type_id: 1)
+Article.create( name: "Test4", category_id: 2, autor_id: nil, body: "Test body for test article4", sources: 'test.article4.dp.ua',  autor: 'Crossroad', type_id: 1)
+
+
