86a28d84463bae722b22f006c8e87742b1e8459e
Max Bulgakov <madmacj@gmail.com>
2013-10-08 23:45:30 +0300

    _

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@
 #Category.create(:name => 'Музыка', :parent_id => 1)
 
 Type.create(:name => 'test article type')
-Article.create( name: "Test", category_id: 2, autor_id: nil, body: "Test body for test article", sources: 'test.article.dp.ua',  autor: 'Crossroad', type_id: 1)
-Article.create( name: "Test2", category_id: 2, autor_id: nil, body: "Test body for test article2", sources: 'test.article2.dp.ua',  autor: 'Crossroad', type_id: 1)
-Article.create( name: "Test3", category_id: 2, autor_id: nil, body: "Test body for test article3", sources: 'test.article3.dp.ua',  autor: 'Crossroad', type_id: 1)
-Article.create( name: "Test4", category_id: 2, autor_id: nil, body: "Test body for test article4", sources: 'test.article4.dp.ua',  autor: 'Crossroad', type_id: 1)
+Article.create( :name => "Test", :category_id => 2, :autor_id => nil,:body => "Test body for test article", :sources => 'test.article.dp.ua',  :autor =>  'Crossroad', :type_id =>1)
+Article.create( :name => "Test2", :category_id => 2, :autor_id => nil, :body => "Test body for test article2", :sources => 'test.article2.dp.ua',  :autor => 'Crossroad', :type_id => 1)
+Article.create( :name => "Test3", :category_id => 2, :autor_id => nil, :body => "Test body for test article3", :sources => 'test.article3.dp.ua',  :autor => 'Crossroad', :type_id => 1)
+Article.create( :name => "Test4", :category_id => 2, :autor_id => nil, :body => "Test body for test article4", :sources => 'test.article4.dp.ua',  :autor => 'Crossroad', :type_id => 1)
 
 
