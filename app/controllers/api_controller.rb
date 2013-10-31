31fa2b76c7f9fbbfd4da260ac3b80ed29bae5cec
Max Bulgakov <madmacj@gmail.com>
2013-10-31 16:39:37 +0200

    fix bugs

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -37,14 +37,20 @@ class ApiController < ApplicationController
   end
 
   def new_type
-    category_id = params[:category_id]
-    section = params[:section]
-    type_name = params[:type_name]
+    category_id = params['category_id']
+    section = params['section']
+    type_name = params['type_name']
     @type = Type.new(:category_id => category_id, :section_type => section, :name => type_name)
-    @type.save
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @type}
+    if @type.save
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @type}
+      end
+    else
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: {:error => @type.errors.to_json}}
+      end
     end
   end
 
